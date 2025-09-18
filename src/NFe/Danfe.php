f4024a08507e3acd66acff18543327e875335962
Victor Pereira <victor.smp13@gmail.com>
2025-09-18 09:36:31 -0300

    Adiciona flag para exibir o nº do pedido de compra (#32)
    * flag para exibir o nº do item no pedido de compra
    
    * flag para exibir o nº do pedido de compra
    
    ---------
    
    Co-authored-by: João Gabriel <joao.royosilva@gmail.com>
    Co-authored-by: Victor Pereira <victor.pereira@qive.com.br>

--- src/NFe/Danfe.php
+++ src/NFe/Danfe.php
@@ -54,6 +54,20 @@ class Danfe extends Common
      */
     protected $exibirTextoFatura = false;
     /**
+     * Parâmetro do controle se deve exibir o número do item no pedido de compra
+     * na descrição do produto
+     *
+     * @var boolean
+     */
+    public $exibirNumeroItemPedido = false;
+    /**
+     * Parâmetro do controle se deve exibir o número do pedido de compra na descricao
+     * do produto
+     *
+     * @var boolean
+     */
+    public $exibirNumeroPedidoCompra = false;
+    /**
      * Parâmetro do controle se deve concatenar automaticamente informações complementares
      * na descrição do produto, como por exemplo, informações sobre impostos.
      * @var boolean
@@ -2725,6 +2739,15 @@ class Danfe extends Common
         if ($this->descProdQuebraLinha) {
             $texto = str_replace(";", "\n", $texto);
         }
+
+        if ($this->exibirNumeroPedidoCompra && !empty($itemProd->getElementsByTagName('xPed')->item(0)->nodeValue)) {
+            $texto .= " (Pedido " . $itemProd->getElementsByTagName('xPed')->item(0)->nodeValue . ")";
+        }
+
+        if ($this->exibirNumeroItemPedido && !empty($itemProd->getElementsByTagName('nItemPed')->item(0)->nodeValue)) {
+            $texto .= " (ITEM " . $itemProd->getElementsByTagName('nItemPed')->item(0)->nodeValue . ")";
+        }
+
         return $texto;
     }
 
